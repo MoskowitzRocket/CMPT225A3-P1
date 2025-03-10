776921d4a2c433bebc2eed0f2d8b2b62eed31583
moskowitzrocket <kalebug@gmail.com>
2025-03-09 21:04:43 -0700

    yuh

--- TestIndPQ.cpp
+++ TestIndPQ.cpp
@@ -8,28 +8,36 @@ int main() {
 	// IndPQ ipq;
 	IndPQ ipq;
 
-	ipq.insert("Task A", 2);
-	ipq.insert("Task B", 4);
-	ipq.insert("Task C", 6);
-	ipq.insert("Task D", 8);
-	ipq.insert("Task E", 7);
-	ipq.insert("Task F", 9);
-	ipq.insert("Task G", 12);
+	// ipq.insert("Task A", 2);
+	// ipq.insert("Task B", 4);
+	// ipq.insert("Task C", 6);
+	// ipq.insert("Task D", 8);
+	// ipq.insert("Task E", 7);
+	// ipq.insert("Task F", 9);
+	// ipq.insert("Task G", 12);
 
 
-	ipq.display();
+	// ipq.display();
 
-	cout << "\n\n\n Deleting Min\n\n\n";
+	// cout << "\n\n\n Deleting Min\n\n\n";
 
-	ipq.deleteMin();
+	// ipq.deleteMin();
+	// ipq.ddisplay();
+
+	// cout << "\n\n\n Updating Priority\n\n\n";
+
+	// ipq.updatePriority("Task C", 1);
+	// ipq.ddisplay();
+
+	ipq.insert("Saffat",4);
 	ipq.ddisplay();
 
-	cout << "\n\n\n Updating Priority\n\n\n";
-
-	ipq.updatePriority("Task C", 1);
+	ipq.insert("Bryce",3);
+	
 	ipq.ddisplay();
 
-
+	ipq.updatePriority("Saffat",1);
+	ipq.ddisplay();
 
 
 	return 0;
