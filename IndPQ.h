24372436f16fe02b0dba37f8eb1855e4c246d354
Kale Moskowitz <kalebug@gmail.com>
2025-03-10 10:41:46 -0700

    Formatted

--- IndPQ.h
+++ IndPQ.h
@@ -15,28 +15,29 @@ public:
     // —-> constructor creating an empty IndPQ.
     IndPQ() : hmap(), heap(hmap), currentSize(0) {}
 
-    // --> Insert taskid with priority p. 
-    void insert(const std::string& taskid, int p)
+    // --> Insert taskid with priority p.
+    void insert(const std::string &taskid, int p)
     {
         HeapNode newNode(p, taskid);
         int index = heap.insert(newNode);
         currentSize++;
-        hmap.insert(taskid, index);  // Store the heap index in hmap
-
+        hmap.insert(taskid, index); // Store the heap index in hmap
     }
 
     //  --> Remove and return (a reference to) a task ID with smallest priority.
-    std::string& deleteMin() {
+    std::string &deleteMin()
+    {
 
-		std::string& index = getMin();
-        //minTaskBuffer = index;
-		hmap.remove(index);
+        std::string &index = getMin();
+        // minTaskBuffer = index;
+        hmap.remove(index);
         heap.deleteMin();
         return index;
     }
 
     //  --> Return (a reference to) a task ID with the smallest priority.
-    std::string& getMin() {
+    std::string &getMin()
+    {
         HeapNode minNode = heap.findMin();
         minTaskBuffer = minNode.taskid;
 
@@ -44,37 +45,40 @@ public:
     }
 
     //  --> change the priority for taskid to p.
-    void updatePriority(const std::string& taskid, int p) {
+    void updatePriority(const std::string &taskid, int p)
+    {
         int index = hmap.getValue(taskid);
-		heap.setPriority(index, p);
+        heap.setPriority(index, p);
 
-		//hmap.setValue(taskid, index);
-		   
-		
+        // hmap.setValue(taskid, index);
     }
 
     // --> remove taskid from the PQ
-    void remove(const std::string& tid) {
-               
+    void remove(const std::string &tid)
+    {
     }
 
     // --> Return true if the PQ is empty, otherwise false.
-    bool isEmpty() {
+    bool isEmpty()
+    {
         return heap.isEmpty();
     }
 
     //  --> Return the number of tasks in the PQ.
-    int size() {
-		return currentSize;
+    int size()
+    {
+        return currentSize;
     }
 
     //  --> Remove all tasks from the PQ.
-    void clear() {
+    void clear()
+    {
         currentSize = 0;
     }
 
     // —-> prints out the queue contents.
-    void display() {
+    void display()
+    {
         heap.display();
         hmap.display();
     }
@@ -87,7 +91,6 @@ public:
     }
 
 private:
-
     std::string minTaskBuffer;
 
     int currentSize;
@@ -100,8 +103,6 @@ private:
         HeapNode(int p, string id) : priority(p), taskid(id) {}
     };
 
-    
-
     class HMap
     {
     public:
@@ -110,7 +111,7 @@ private:
             makeEmpty();
         }
 
-        bool contains(const string& x) const
+        bool contains(const string &x) const
         {
             return isActive(findPos(x));
         }
@@ -118,7 +119,7 @@ private:
         void makeEmpty()
         {
             currentSize = 0;
-            for (auto& entry : array)
+            for (auto &entry : array)
                 entry.info = EMPTY;
         }
 
@@ -127,7 +128,7 @@ private:
             return currentSize;
         }
 
-        bool insert(const string& x, const int& y)
+        bool insert(const string &x, const int &y)
         {
             // Insert x as active
             int currentPos = findPos(x);
@@ -146,9 +147,9 @@ private:
                 rehash();
 
             return true;
-        }      
+        }
 
-        bool remove(const string& x)
+        bool remove(const string &x)
         {
             int currentPos = findPos(x);
             if (!isActive(currentPos))
@@ -158,17 +159,17 @@ private:
             return true;
         }
 
-        const int& getValue(const string& x) const
+        const int &getValue(const string &x) const
         {
             int pos = findPos(x); // Find the index of the key in the hash table
             return array[pos].value;
         }
 
-		void setValue(const string& x, const int& y)
-		{
-			int pos = findPos(x); // Find the index of the key in the hash table
-			array[pos].value = y;
-		}
+        void setValue(const string &x, const int &y)
+        {
+            int pos = findPos(x); // Find the index of the key in the hash table
+            array[pos].value = y;
+        }
 
         enum EntryType
         {
@@ -179,7 +180,7 @@ private:
 
         void display()
         {
-            for (const auto& entry : array)
+            for (const auto &entry : array)
             {
                 if (entry.info == ACTIVE)
                     cout << "(" << entry.key << ", Index: " << entry.value << ")\n";
@@ -211,12 +212,14 @@ private:
             int value;
             EntryType info;
 
-            HashEntry(const string& e = string{}, EntryType i = EMPTY)
-                : key{ e }, info{ i } {
+            HashEntry(const string &e = string{}, EntryType i = EMPTY)
+                : key{e}, info{i}
+            {
             }
 
-            HashEntry(string&& e, EntryType i = EMPTY)
-                : key{ std::move(e) }, info{ i } {
+            HashEntry(string &&e, EntryType i = EMPTY)
+                : key{std::move(e)}, info{i}
+            {
             }
         };
 
@@ -228,13 +231,13 @@ private:
             return array[currentPos].info == ACTIVE;
         }
 
-        int findPos(const string& x) const
+        int findPos(const string &x) const
         {
             int offset = 1;
             int currentPos = myhash(x);
 
             while (array[currentPos].info != EMPTY &&
-                array[currentPos].key != x)
+                   array[currentPos].key != x)
             {
                 currentPos += offset; // Compute ith probe
                 offset += 2;
@@ -251,76 +254,82 @@ private:
 
             // Create new double-sized, empty table
             array.resize(nextPrime(2 * oldArray.size()));
-            for (auto& entry : array)
+            for (auto &entry : array)
                 entry.info = EMPTY;
 
             // Copy table over
             currentSize = 0;
-            for (auto& entry : oldArray) {
-                if (entry.info == ACTIVE) {
+            for (auto &entry : oldArray)
+            {
+                if (entry.info == ACTIVE)
+                {
                     insert(std::move(entry.key), std::move(entry.value));
                 }
             }
         }
 
-        size_t myhash(const string& x) const
+        size_t myhash(const string &x) const
         {
             static hash<string> hf;
             return hf(x) % array.size();
         }
     };
 
-    class Heap {
+    class Heap
+    {
 
     public:
-        explicit Heap(HMap& hmapref, int capacity = 11) : hmap(hmapref), array(capacity), currentSize{ 0 } {
+        explicit Heap(HMap &hmapref, int capacity = 11) : hmap(hmapref), array(capacity), currentSize{0}
+        {
 
             buildHeap();
         }
-        explicit Heap(HMap& hmapref, const vector<HeapNode>& items) : hmap(hmapref), array(static_cast<int>(items.size()) + 10), currentSize{ static_cast<int>(items.size()) } {
-            for (int i{ 0 }; i < items.size(); ++i) {
+        explicit Heap(HMap &hmapref, const vector<HeapNode> &items) : hmap(hmapref), array(static_cast<int>(items.size()) + 10), currentSize{static_cast<int>(items.size())}
+        {
+            for (int i{0}; i < items.size(); ++i)
+            {
                 array[i + 1] = items[i];
             }
             buildHeap();
         }
 
-        bool isEmpty() const {
+        bool isEmpty() const
+        {
             return currentSize == 0;
         }
 
-        const HeapNode& findMin() const {
+        const HeapNode &findMin() const
+        {
             return array[1];
         }
 
-        int insert(const HeapNode& x) {
+        int insert(const HeapNode &x)
+        {
             if (currentSize + 1 == array.size())
                 array.resize(array.size() * 2);
 
-            //percolate up
+            // percolate up
             int hole = ++currentSize;
             HeapNode copy = x;
 
             array[0] = std::move(copy);
 
-            //percolate up and update hashmanp
-            for (; x.priority < array[hole / 2].priority; hole /= 2) {
+            // percolate up and update hashmanp
+            for (; x.priority < array[hole / 2].priority; hole /= 2)
+            {
                 array[hole] = std::move(array[hole / 2]);
                 cout << "Moving " << array[hole].taskid << " to index " << hole << endl;
                 hmap.setValue(array[hole].taskid, hole);
-
             }
             array[hole] = std::move(array[0]);
             cout << "Inserting " << x.taskid << " at index " << hole << endl;
             hmap.setValue(x.taskid, hole);
 
-
-
-
             return hole;
         }
 
-
-        void deleteMin() {
+        void deleteMin()
+        {
             if (isEmpty())
             {
                 cerr << "Cannot delete min -> heap is empty" << endl;
@@ -331,72 +340,85 @@ private:
             percolateDown(1);
         }
 
-        void makeEmpty() {
+        void makeEmpty()
+        {
             currentSize = 0;
         }
 
-        void display() const {
+        void display() const
+        {
             cout << "Heap (Array Representation): \n";
-            for (int i = 1; i <= currentSize; ++i) {
+            for (int i = 1; i <= currentSize; ++i)
+            {
                 cout << "[" << array[i].priority << ", " << array[i].taskid << "] \n";
             }
             cout << endl;
         }
 
-        void ddisplay() const {
+        void ddisplay() const
+        {
             cout << "Debug Display. CurrentSize: " << currentSize << endl;
 
-            for (int i = 0; i < array.size(); i++) {
+            for (int i = 0; i < array.size(); i++)
+            {
                 cout << "Index " << i << ": [" << array[i].priority << ", " << array[i].taskid << "] \n";
             }
         }
 
-        void setPriority(int index, int p) {
-			array[index].priority = p;
+        void setPriority(int index, int p)
+        {
+            array[index].priority = p;
 
-            if (index > 1 && array[index].priority < array[index / 2].priority) {
+            if (index > 1 && array[index].priority < array[index / 2].priority)
+            {
                 percolateUp(index);
             }
-            else percolateDown(index);
-
+            else
+                percolateDown(index);
         }
 
     private:
-
-        int currentSize; //number of elements in the heap
+        int currentSize; // number of elements in the heap
         vector<HeapNode> array;
-        HMap& hmap;
+        HMap &hmap;
 
-        void buildHeap() {
-            for (int i = currentSize / 2; i > 0; --i) {
+        void buildHeap()
+        {
+            for (int i = currentSize / 2; i > 0; --i)
+            {
                 percolateDown(i);
             }
         }
 
-        void percolateUp(int hole) {
-            
+        void percolateUp(int hole)
+        {
         }
 
-        void percolateDown(int hole) {
+        void percolateDown(int hole)
+        {
             int child;
             HeapNode tmp = std::move(array[hole]);
 
-            for (; hole * 2 <= currentSize; hole = child) {
+            for (; hole * 2 <= currentSize; hole = child)
+            {
                 child = hole * 2;
-                if (child != currentSize && array[child + 1].priority < array[child].priority) {
+                if (child != currentSize && array[child + 1].priority < array[child].priority)
+                {
                     ++child;
                 }
-                if (array[child].priority < tmp.priority) {
-                    array[hole] = std::move(array[child]);\
-						hmap.setValue(array[hole].taskid, hole);
+                if (array[child].priority < tmp.priority)
+                {
+                    array[hole] = std::move(array[child]);
+                    hmap.setValue(array[hole].taskid, hole);
                 }
-                else {
+                else
+                {
                     break;
                 }
             }
 
             array[hole] = std::move(tmp);
-			hmap.setValue(array[hole].taskid, hole);
+            hmap.setValue(array[hole].taskid, hole);
         }
     };
 
